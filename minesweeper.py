2149feab0a8ad9c436265de1f46d4995e93d3dfd
Marcin Hamiga <marcin.hamiga12@gmail.com>
2023-03-04 00:20:56 +0100

    Created the game
    Copied files into the repo

---
+++ minesweeper.py
@@ -0,0 +1,95 @@
+from random import randint
+from copy import deepcopy
+
+def get_number(a: int, b: int, text: str) -> int:
+    while True:
+        number = int(input(text))
+        if a <= number <= b:
+            return number
+        else:
+            print("Enter a valid number!")
+
+def lay_mines(a: int, b: int, mines_num: int) -> set:
+    mines = set()   # Tworzymy nowy set (Nie, zrobienie tego klamerkami NIE działa. Tworzycie wtedy słownik a nie set.)
+    for x in range(0, mines_num + 1):
+        mines.add((randint(0, a-1), randint(0, b-1)))   # Set ma metodę add zamiast append. Do zapamiętania.
+    return mines
+
+def number_of_neighbouring_mines(x: int, y: int, board: list) -> int:
+    if board[x][y] == "B":
+        return -1
+    neigh_mines = 0
+    for n in range(x-1, x+2):   # Pętla sprawdzająca wiersze
+        for m in range(y-1, y+2):   # Pętla sprawdzająca kolumny
+            if 0 <= n < len(board) and 0 <= m < len(board[n]):
+                if board[n][m] == "B":
+                    neigh_mines += 1
+            else:
+                continue   
+    return neigh_mines 
+
+def create_board(m: int, n: int, mines: set):
+    # Generowanie pustej listy board
+    board = []
+    for row in range(m):    # Pętla wierszy
+        board.append([])    # Dodawanie list do listy, aby utworzyć listę dwuwymiarową
+        for col in range(n):    # Pętla kolumn
+            board[row].append(0)    # Wprowadzanie wartości 0 do każdej komórki w liście
+    
+    mask = deepcopy(board)  # Stworzenie maski poprzez głęboką kopię listy board
+    
+    for mine in mines:  # Pętla służąca rozlokowaniu min
+        x, y = mine # Rozpakowanie krotki mine
+        board[x][y] = "B"   # Rozlokowanie min na planszy
+
+    # Tutaj dzieje się kongo
+    for i, row in enumerate(board): 
+        for j, col in enumerate(row):
+            number = number_of_neighbouring_mines(i, j, board) # Szukamy ile bomb ma dookoła siebie każde pole
+            if number == -1:
+                continue    # Jak pole jest bombą to nic nie robimy, zostawiamy tam ładnie 'B'
+            else:
+                row[j] = number # Jak pole nie jest bomba, to ładujemy tam liczbę bomb dookoła
+    return board, mask
+
+def reveal_fields(m: int, n: int, x: int, y: int, board: list, mask: list):
+    if board[x][y] == "B": # Jak bum bum no to przegrywasz byq
+        mask[x][y] = 1
+        print("You lost!")
+        return -1
+    if mask[x][y] == 1: # Jak pole już było odkryte, to zakańczamy wykonanie funkcji, aby uniknąć nieskończonej rekurencji
+        return 0
+    
+    if board[x][y] == 0:    # Jak pole nie ma dookoła min, no to generalnie bajlando. Lecimy na pole po lewej, po prawej, poniżej i powyżej.
+        mask[x][y] = 1  # Oznaczamy pole jako odkryte
+        if x - 1 > -1:
+            reveal_fields(m, n, x - 1, y, board, mask)
+        if x + 1 <= m-1:
+            reveal_fields(m, n, x + 1, y, board, mask)
+        if y - 1 > -1:
+            reveal_fields(m, n, x, y - 1, board, mask)
+        if y + 1 <= n-1:
+            reveal_fields(m, n, x, y + 1, board, mask)
+    if 0 < board[x][y] < 9: # Jak pole ma dookoła miny, to oznaczamy je jako odkryte ale nie lecimy już na następne pola
+        mask[x][y] = 1 
+
+def print_board(m: int, n: int, board: list, mask: list):
+    rows = 1
+    cols = 1
+    print(f"{'':<3}", end="")   # Dużo printów, a generalnie <3 pozwala nam na sztywno ustawić, że wyrażenie w klamerce ma zostać zapisane na trzech miejscach w terminalu.
+    for x in range(m):
+        print(f"{f'{cols}':<3}", end="")    # Inna liczba, np <4 ustawi na sztywno, że ma zająć 4 miejsca.
+        cols += 1
+    print()
+    for row_b, row_m in zip(board, mask):   # Zipujemy sobie dwie listy, zeby móc iterować po obu na raz.
+        print(f"{f'{rows}':<3}", end="")
+        rows += 1
+        for col_b, col_m in zip(row_b, row_m):
+            if col_m == 0:
+                print(f"{'?':<3}", end="")
+            else:
+                if col_b == 0:
+                    print(f"{' ':<3}", end="")
+                else:
+                    print(f"{col_b:<3}", end="")
+        print()
---
+++ saper.py
@@ -0,0 +1,30 @@
+from minesweeper import *
+import platform
+import os
+
+def saper():
+    is_running = True
+    m = get_number(8, 30, f"Enter a number(8-30):")
+    n = get_number(8, 24, f"Enter a number(8-24):")
+    
+    max_mines = (m-1) * (n-1)
+    num_mines = get_number(10, max_mines, f"Enter a number of mines(10-{max_mines})")
+
+    mines = lay_mines(m, n, num_mines)
+
+    board, mask = create_board(m, n, mines)
+    print_board(m, n, board, mask)
+    while is_running:
+        x = get_number(0, m, f"Enter a row (1-{m}): ") - 1
+        y = get_number(0, n, f"Enter a column (1-{n}): ") - 1
+
+        if platform.system().lower() == "linux":
+            os.system("clear")
+        if platform.system().lower() == "windows":
+            os.system("cls")
+
+        lose = reveal_fields(m, n, x, y, board, mask)
+        if lose == -1:
+            is_running = False
+        print_board(m, n, board, mask)
+saper()
